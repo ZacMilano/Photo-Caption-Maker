d9efa8b68e6927d2fb31a594b57c2148ada043ea
ZacMonroe <zmonroe0820@gmail.com>
2017-12-20 16:46:15 -0500

    Working i-o stream with GUI; began work on day calculator The GUI now supports taking the user input and putting it into the ouput section. It puts a line break between each output caption. I had to use a lambda expression for this, which was kinda cool. Output font is now Avenir, whih looks nice. There is a font size slider that doesn't really work yet.

--- date_to_day_of_week_calculator.py
+++ date_to_day_of_week_calculator.py
@@ -1,2 +1,16 @@
 def determine_day(input):
-  pass
+  if not type(input) == str:
+    print("Error in determine_day: input is not a string.")
+    return -1
+  splitting_chars = [" ", "/", "\\", "-", "_"]
+  output_list=[]
+  for char in splitting_chars:
+    output_list.append(input.split(char))
+  for n in range(len(output_list)):
+    print(output_list[n])
+    if output_list[n][0] == input:
+      output_list.pop(n)
+      print(n)
+  return output_list
+
+print(determine_day("hey there"))
--- main_file.py
+++ main_file.py
@@ -5,7 +5,7 @@ class Main_Window:
   def __init__(self):
     self.main_bg = "#21252B"
     self.gui_text_col = "#9da5b4"
-    self.button_col = "#30353e" #282C34
+    self.button_col = "#30353e"  #"#282C34"
 
 
     self.master = tkinter.Tk()
@@ -18,24 +18,32 @@ class Main_Window:
                                       text="Who?",
                                       bg=self.main_bg,
                                       fg=self.gui_text_col)
-    self.time_label = tkinter.Label(self.master,
-                                    text = "When?",
-                                    bg=self.main_bg,
-                                    fg=self.gui_text_col)
     self.what_was_done_label = tkinter.Label(self.master,
                                              text = "What?",
                                              bg=self.main_bg,
                                              fg=self.gui_text_col)
+    self.time_label = tkinter.Label(self.master,
+                                    text = "When?",
+                                    bg=self.main_bg,
+                                    fg=self.gui_text_col)
 
     self.person_entry = tkinter.Entry(self.master, bg=self.gui_text_col)
-    self.time_entry = tkinter.Entry(self.master, bg=self.gui_text_col)
     self.what_was_done_entry = tkinter.Entry(self.master, bg=self.gui_text_col)
-    '''Creation of widget elements'''
+    self.time_entry = tkinter.Entry(self.master, bg=self.gui_text_col)
+    # Creation of widget elements
 
+    self.highest_row_index = 1
+    # For use in adding captions with add_caption
+    self.total_columns = 6
+    # For use in columnspan cases
 
     self.action_button = tkinter.Button(self.master,
                                         text = "GO!",
-                                        command=self.add_caption,
+                                        command=lambda: self.add_caption(
+                                          self.person_entry.get(),
+                                          self.what_was_done_entry.get(),
+                                          self.time_entry.get()
+                                        ),
                                         fg=self.gui_text_col,
                                         bg=self.button_col)
 
@@ -43,13 +51,13 @@ class Main_Window:
     '''I added pady only to the first column because it stays like that
     for the other columns.'''
     self.person_entry.grid(row=0, column=1, sticky="we", padx = 10)
-    self.time_label.grid(row=0, column=2, padx = 10)
-    self.time_entry.grid(row=0, column=3, sticky="we", padx = 10)
-    self.what_was_done_label.grid(row=0, column=4, padx = 10)
-    self.what_was_done_entry.grid(row=0, column=5, sticky="we", padx = 10)
+    self.what_was_done_label.grid(row=0, column=2, padx = 10)
+    self.what_was_done_entry.grid(row=0, column=3, sticky="we", padx = 10)
+    self.time_label.grid(row=0, column=4, padx = 10)
+    self.time_entry.grid(row=0, column=5, sticky="we", padx = 10)
 
-    self.total_columns = 6
-    '''For use in columnspan cases'''
+
+
 
     self.action_button.grid(row=1, column=0,
                             sticky = "we",
@@ -62,24 +70,48 @@ class Main_Window:
     self.master.columnconfigure(3, weight = 2)
     self.master.columnconfigure(4, weight = 1)
     self.master.columnconfigure(5, weight = 2)
-    '''Makes the columns scale according to the window width'''
+    # Makes the columns scale according to the window width
 
-    self.highest_row_index = 1
-    '''For use in adding captions with add_caption'''
+
 
     self.master.mainloop()
 
-  def add_caption(self):#str_0, str_1, str_2):
-    self.highest_row_index += 1
-    new_text = tkinter.Text(self.master,
-                            wrap="word",
-                            width=50,
-                            height=2,
-                            font=("Comic Sans MS", 12, "bold"),
-                            bg=self.gui_text_col)
+  def add_caption(self, str_0, str_1, str_2):
+    if not self.highest_row_index > 1:
+      self.highest_row_index = 3
+      self.font_size_slider = tkinter.Scale(self.master,
+                                            from_=5,
+                                            to=40,
+                                            orient="horizontal",
+                                            label="Font Size",
+                                            bg=self.button_col,
+                                            fg=self.gui_text_col,
+                                            highlightthickness=0,
+                                            relief="solid",
+                                            sliderrelief="flat",
+                                            tickinterval=5)
+      self.font_size_slider.set(14)
+      self.font_size_slider.grid(row=self.highest_row_index-1,
+                                 columnspan=self.total_columns,
+                                 sticky="we")
+      self.new_text = tkinter.Text(self.master,
+                                   wrap="word",
+                                   width=50,
+                                   height=10,
+                                   padx=5,
+                                   pady=5,
+                                   font=("Avenir",
+                                         self.font_size_slider.get(),
+                                         "italic", "bold"),
+                                   bg=self.gui_text_col)
+      self.new_text.grid(row=self.highest_row_index,
+                         columnspan=self.total_columns,
+                         sticky="we")
 
-    new_text.grid(row=self.highest_row_index,
-                  columnspan=self.total_columns)
+    self.new_text.insert(1.0,
+                         "{} did {} on {}.\n".format(
+                           str_0, str_1, str_2
+                         ))
 
 
 a = Main_Window()
