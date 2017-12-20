93981f731c0d2946956ca7bd242b5615f4652ae2
ZacMonroe <zmonroe0820@gmail.com>
2017-12-19 21:40:11 -0500

    Vastly improved GUI Generalized the GUI for window scaling and whatnot. Also put the button below the input fields.

--- main_file.py
+++ main_file.py
@@ -46,6 +46,7 @@ class Main_Window:
   def __init__(self):
     self.master = tkinter.Tk()
     self.master.title("Caption Maker")
+    # self.master.geometry('{}x{}'.format(800, 50))
 
     self.person_label = tkinter.Label(self.master, text = "Who?")
     self.time_label = tkinter.Label(self.master, text = "When?")
@@ -57,16 +58,33 @@ class Main_Window:
 
     self.action_button = tkinter.Button(self.master, text = "GO!")
 
-    self.person_label.pack(side = "left")
-    self.person_entry.pack(side = "left")
-    self.time_label.pack(side = "left")
-    self.time_entry.pack(side = "left")
-    self.what_was_done_label.pack(side = "left")
-    self.what_was_done_entry.pack(side = "left")
+    self.person_label.grid(row=0, column=0, pady=10)  # No need to add pady to
+    self.person_entry.grid(row=0, column=1, sticky = "we")  # every column in
+    self.time_label.grid(row=0, column=2)                   # this row.
+    self.time_entry.grid(row=0, column=3, sticky = "we")
+    self.what_was_done_label.grid(row=0, column=4)
+    self.what_was_done_entry.grid(row=0, column=5, sticky = "we")
 
-    self.action_button.pack(side = "bottom")
+    self.action_button.grid(row=2, column=0,
+                            sticky = "we",
+                            columnspan=6,
+                            pady=5)
+
+    self.master.columnconfigure(1, weight = 1)
+    self.master.columnconfigure(3, weight = 1)
+    self.master.columnconfigure(5, weight = 1)
+
 
     self.master.mainloop()
 
+  def add_caption(str_0, str_1, str_2):
+    pass
+
 
 a = Main_Window()
+
+
+
+
+
+#
